da9a2ac514eb44293f99c898af70a84865f87eb4
Jeff Langr <jeff@langrsoft.com>
2023-10-04 12:11:43 -0600

    fails on my machine

--- src/test/java/iloveyouboss/service/ACriterionService.java
+++ src/test/java/iloveyouboss/service/ACriterionService.java
@@ -1,25 +1,18 @@
 package iloveyouboss.service;
 
-import iloveyouboss.data.AnswerData;
 import iloveyouboss.data.CriterionData;
 import iloveyouboss.data.YesNoQuestionData;
-import iloveyouboss.domain.Answer;
 import iloveyouboss.domain.Criterion;
 import iloveyouboss.domain.questions.YesNoQuestion;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static iloveyouboss.domain.questions.YesNoQuestion.Yes;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,10 +21,10 @@ class ACriterionService {
    YesNoQuestionData questionData;
    @Mock
    CriterionData criterionData;
-   @InjectMocks
 
    YesNoQuestion question = new YesNoQuestion(42, "When?");
    Criterion criterion = new Criterion(1, 42, Yes);
+   @InjectMocks
    CriterionService criterionService;
 
    @BeforeEach
