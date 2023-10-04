fad7f16912ccd6c07de1428aa60e653814425f30
Jeff Langr <jeff@langrsoft.com>
2023-10-04 17:46:20 -0600

    cleanup of criterion service tests

--- src/test/java/iloveyouboss/service/ACriterionService.java
+++ src/test/java/iloveyouboss/service/ACriterionService.java
@@ -1,9 +1,13 @@
 package iloveyouboss.service;
 
-import iloveyouboss.data.CriterionData;
+import iloveyouboss.data.ChoiceQuestionData;
 import iloveyouboss.data.YesNoQuestionData;
 import iloveyouboss.domain.Criterion;
+import iloveyouboss.domain.InvalidAnswerException;
+import iloveyouboss.domain.questions.ChoiceQuestion;
 import iloveyouboss.domain.questions.YesNoQuestion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,34 +15,74 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
+
+import static iloveyouboss.domain.questions.YesNoQuestion.No;
 import static iloveyouboss.domain.questions.YesNoQuestion.Yes;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ACriterionService {
-   @InjectMocks
-   CriterionService criterionService;
-
-   @Mock
-   YesNoQuestionData questionData;
-   @Mock
-   CriterionData criterionData;
-
    @Nested
-   class GetQuestion {
-      YesNoQuestion question = new YesNoQuestion(42, "When?");
-      Criterion criterion = new Criterion(1, question.id(), Yes);
+   class GivenYesNoCriterion {
+      @InjectMocks
+      CriterionService criterionService;
+
+      @Mock
+      YesNoQuestionData questionData;
+
+      YesNoQuestion question = new YesNoQuestion(1, "?");
+      Criterion yesCriterion = new Criterion(1, question.id(), Yes);
+
+      @BeforeEach
+      public void create() {
+         when(questionData.get(question.id())).thenReturn(question);
+      }
 
       @Test
-      void returnsQuestionAssociatedWithId() {
-         when(criterionData.get(criterion.id())).thenReturn(criterion);
-         when(questionData.get(criterion.questionId())).thenReturn(question);
+      void isMetByAnswerMatchingItsExpectedAnswer() {
+         assertTrue(criterionService.isMetBy(yesCriterion, Yes));
+      }
 
-         var retrieved = criterionService.getQuestion(criterion.id());
-
-         assertEquals(question, retrieved);
+      @Test
+      void isNotMetByAnswerMismatchingItsExpectedAnswer() {
+         assertFalse(criterionService.isMetBy(yesCriterion, No));
       }
    }
 
+   @Nested
+   class GivenChoiceQuestion {
+      @InjectMocks
+      CriterionService criterionService;
+
+      @Mock
+      ChoiceQuestionData questionData;
+
+      ChoiceQuestion question =
+         new ChoiceQuestion(1, "?", List.of("eeny", "meeny", "miny", "moe"));
+      Criterion eenyCriterion = new Criterion(question.id(), "eeny");
+
+      @BeforeEach
+      public void create() {
+         when(questionData.get(question.id())).thenReturn(question);
+      }
+
+      @Test
+      void isMetByAnswerMatchingItsExpectedAnswer() {
+         assertTrue(criterionService.isMetBy(eenyCriterion, "eeny"));
+      }
+
+      @Test
+      void isNotMetByAnswerMismatchingItsExpectedAnswer() {
+         assertFalse(criterionService.isMetBy(eenyCriterion, "moe"));
+      }
+
+      @Test
+      void throwsWhenAnswerDoesNotMatchAvailableChoices() {
+         assertThrows(InvalidAnswerException.class,
+            () -> criterionService.isMetBy(eenyCriterion, "invalid answer"));
+      }
+   }
 }
---
+++ src/test/java/iloveyouboss/service/ACriterionService_WhenGettingQuestion.java
@@ -0,0 +1,40 @@
+package iloveyouboss.service;
+
+import iloveyouboss.data.CriterionData;
+import iloveyouboss.data.YesNoQuestionData;
+import iloveyouboss.domain.Criterion;
+import iloveyouboss.domain.questions.YesNoQuestion;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static iloveyouboss.domain.questions.YesNoQuestion.Yes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ACriterionService_WhenGettingQuestion {
+   @InjectMocks
+   CriterionService criterionService;
+
+   @Mock
+   YesNoQuestionData questionData;
+   @Mock
+   CriterionData criterionData;
+
+   YesNoQuestion question = new YesNoQuestion(42, "When?");
+   Criterion criterion = new Criterion(1, question.id(), Yes);
+
+   // TODO why does this intermittently fail
+   @Test
+   void returnsQuestionAssociatedWithId() {
+      when(criterionData.get(criterion.id())).thenReturn(criterion);
+      when(questionData.get(criterion.questionId())).thenReturn(question);
+
+      var retrieved = criterionService.getQuestion(criterion.id());
+
+      assertEquals(question, retrieved);
+   }
+}
