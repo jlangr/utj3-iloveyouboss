be640515513d72a3c0d6b736e842f759679055e6
Jeff Langr <jeff@langrsoft.com>
2023-10-04 09:28:01 -0600

    fix stats stuff

--- src/main/java/iloveyouboss/service/StatisticsService.java
+++ src/main/java/iloveyouboss/service/StatisticsService.java
@@ -1,46 +1,20 @@
 package iloveyouboss.service;
 
-import iloveyouboss.data.AnswerData;
-import iloveyouboss.data.CriterionData;
-import iloveyouboss.data.YesNoQuestionData;
-import iloveyouboss.domain.Answer;
-import iloveyouboss.domain.Criterion;
 import iloveyouboss.domain.StatisticsCompiler;
-import iloveyouboss.domain.questions.YesNoQuestion;
-
 import java.util.Map;
 
 public class StatisticsService {
-   final AnswerService answerService = new AnswerService(
-      new AnswerData(),
-      new CriterionService(new YesNoQuestionData(), new CriterionData()));
+   final AnswerService answerService;
+   final StatisticsCompiler statisticsCompiler = new StatisticsCompiler();
 
-   final static YesNoQuestionData questionData = new YesNoQuestionData();
-   final static CriterionData criterionData = new CriterionData();
-   final static AnswerData answerData = new AnswerData();
+   public StatisticsService(AnswerService answerService) {
+      this.answerService = answerService;
+   }
 
    // START_HIGHLIGHT
-   final StatisticsCompiler statisticsCompiler = new StatisticsCompiler();
-   // END_HIGHLIGHT
-
    public Map<String, Map<String, Integer>> answerHistogram() {
-      // START_HIGHLIGHT
       var answers = answerService.retrieveAllAnnotated();
-      // END_HIGHLIGHT
       return statisticsCompiler.answerCountsByQuestionText(answers);
    }
-
-   public static void main(String[] args) {
-      var whoId = questionData.add(new YesNoQuestion("Who?"));
-      var whenId = questionData.add(new YesNoQuestion("When?"));
-      var whoCriterionId = criterionData.add(new Criterion(whoId, "everyone"));
-      var whenCriterionId = criterionData.add(new Criterion(whenId, "now"));
-      answerData.add(new Answer(whoCriterionId, "everyone"));
-      answerData.add(new Answer(whoCriterionId, "everyone"));
-      answerData.add(new Answer(whoCriterionId, "no one"));
-      answerData.add(new Answer(whenCriterionId, "now"));
-      answerData.add(new Answer(whenCriterionId, "later"));
-
-      System.out.println(new StatisticsService().answerHistogram());
-   }
+   // END_HIGHLIGHT
 }
