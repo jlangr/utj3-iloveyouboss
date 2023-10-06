9cf71366de256c0938798d198f0a0209c169c5bf
Jeff Langr <jeff@langrsoft.com>
2023-10-06 13:51:36 -0600

    compare questions without timestamps

---
+++ src/test/java/iloveyouboss/domain/utils/ComparisonUtils.java
@@ -0,0 +1,30 @@
+package iloveyouboss.domain.utils;
+
+import iloveyouboss.domain.Question;
+import iloveyouboss.domain.questions.YesNoQuestion;
+
+import java.time.Instant;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+// well, things like AssertJ make this much easier
+public class ComparisonUtils {
+   public static Question withoutTimestamp(Question question) {
+      // TODO
+//      return switch (question) {
+//         case YesNoQuestion q -> new YesNoQuestion(q.id(), q.text(), Instant.MIN);
+//         case ChoiceQuestion q -> q;
+//         default -> null;
+//      };
+      if (question.getClass() == YesNoQuestion.class)
+         return new YesNoQuestion(question.id(), question.text(), Instant.MIN);
+      return question;
+   }
+
+   public static List<Question> withoutTimestamps(List<Question> list) {
+      return list.stream()
+         .map(ComparisonUtils::withoutTimestamp)
+         .collect(toList());
+   }
+}
